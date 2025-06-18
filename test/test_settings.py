f5c698e9fc57779137e92b7a2ff635ef9380ddba
danila nikitin <danila.n2015@yandex.ru>
2025-06-18 20:11:05 +0300

    add mark test settings local label

--- test/test_settings.py
+++ test/test_settings.py
@@ -2,6 +2,7 @@ from app.setting.setting import Settings
 from .conftest import *
 
 @pytest.mark.system
+@pytest.mark.local
 class TestSettings:
     def test_load_settings(self, test_settings: Settings):
         assert test_settings.ENV == 'test'
